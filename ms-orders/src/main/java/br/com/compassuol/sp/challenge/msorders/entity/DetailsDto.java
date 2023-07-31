e0c0dcdd2f2fb38dfd4c756f23ce8f545a0c519a
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2023-07-31 14:37:57 -0300

    create dto for product details

---
+++ ms-orders/src/main/java/br/com/compassuol/sp/challenge/msorders/entity/DetailsDto.java
@@ -0,0 +1,11 @@
+package br.com.compassuol.sp.challenge.msorders.entity;
+
+import java.math.BigDecimal;
+
+public class DetailsDto {
+
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
