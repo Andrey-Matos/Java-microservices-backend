6b3667a8f113eb07090d43562893d756d4207418
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2023-07-31 14:37:32 -0300

    add client injection

--- ms-orders/src/main/java/br/com/compassuol/sp/challenge/msorders/service/OrderService.java
+++ ms-orders/src/main/java/br/com/compassuol/sp/challenge/msorders/service/OrderService.java
@@ -1,21 +1,31 @@
 package br.com.compassuol.sp.challenge.msorders.service;
 
 
+import br.com.compassuol.sp.challenge.msorders.entity.DetailedOrderDto;
+import br.com.compassuol.sp.challenge.msorders.entity.DetailsDto;
 import br.com.compassuol.sp.challenge.msorders.entity.OrderEntity;
 import br.com.compassuol.sp.challenge.msorders.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
 public class OrderService {
 
     public  OrderService(){}
+
+    @Autowired
+    private ProductDetailsClient productClient;
+
+    @Autowired
     private OrderRepository orderRepository;
     public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
@@ -47,4 +57,25 @@ public class OrderService {
         return pageIterator.getContent();
     }
 
+    public DetailedOrderDto getDetailedOrderById(Long id) {
+        List<DetailsDto> detailsList = new ArrayList<>();
+        DetailsDto details;
+        DetailedOrderDto detailedOrder = new DetailedOrderDto();
+
+        Optional<OrderEntity> order = orderRepository.findById(id);
+
+        for (Long i : order.get().getProducts()) {
+            if (productClient.getProductById(i) != null) {
+                detailsList.add(productClient.getProductById(i));
+            }
+        }
+
+        detailedOrder.setId(order.get().getId());
+        detailedOrder.setUserId(order.get().getId());
+        detailedOrder.setProductsDetails(detailsList);
+        detailedOrder.setDeliveryAddress(order.get().getEnderecoEntrega());
+        detailedOrder.setStatus(order.get().getStatus());
+
+        return detailedOrder;
+    }
 }
